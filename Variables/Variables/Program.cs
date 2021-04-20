6175b1ded47e3f4b5aa36b434e2b53c3e60a75e0
Michael Clark <michaelclarknashville@protonmail.com>
2021-04-20 15:50:11 -0500

    defining arrays

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -4,18 +4,27 @@ using Variables.Math;
 
 namespace Variables
 {
-    class MainClass
+    class Program
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            var numbers = new int[3] { 2, 2, 3 };
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3] { true, true, false };
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+
+            Console.WriteLine(names[1]);
         }
     }
 }
