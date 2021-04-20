59caec0d1a50317b54098997adecb98ee6061cdd
Michael Clark <michaelclarknashville@protonmail.com>
2021-04-20 17:02:32 -0500

    understanding the way value and reference types use memory

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -4,34 +4,26 @@ using Variables.Math;
 
 namespace Variables
 {
-    public enum ShippingMethod
-    {
-        RegularAirMail = 1,
-        RegisteredAirMail = 2,
-        Express = 3
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var method = ShippingMethod.Express;
-            Console.WriteLine((int)method);
-            //prints 3
+            //Int is a Value Type, a's value is copied to b
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a,b));
+            //prints a: 10, b:11
 
-            var methodId = 3;
-            Console.WriteLine((ShippingMethod)methodId);
-            //prints Express
-
-            Console.WriteLine(method.ToString());
-            //prints Express
-
-            Console.WriteLine(method);
-            //prints Express, default of Cons.WrLine is toString()
-
-            var methodName = "Express";
-            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-            Console.WriteLine(shippingMethod);
-            //prints Express
+            //Arrays are a Reference Type
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            //array1's value is stored in memory in the heap at an "address"
+            //array1 is defined in the stack as having the value of that "address"
+            //array2's value is array1's value is the same as the "address" value
+            //they REFERENCE the address
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
 
 
         }
