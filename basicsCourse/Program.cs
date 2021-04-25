0586d2ab2449f5818d3bcbde8540a0c14a10cb8c
Michael Clark <michaelclarknashville@protonmail.com>
2021-04-24 19:01:35 -0500

    exercise 1, stopwatch, done

---
+++ basicsCourse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Lists.Math;
+using System.Collections.Generic;
+
+
+namespace Lists
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+    }
+
+}
---
+++ intermediateCourse/Intermediate/Intermediate/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Intermediate
+{
+    class StopWatch
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+        private TimeSpan duration;
+        private string startOrStop = "start";
+        private bool running = false;
+        private TimeSpan total;
+
+        public void Start()
+        {
+            this.startTime = DateTime.Now;
+            this.startOrStop = "stop";
+            this.running = true;
+            Console.WriteLine(this.startTime);
+        }
+
+        public void Stop()
+        {
+            this.endTime = DateTime.Now;
+            this.startOrStop = "start";
+            this.running = false;
+            this.duration = this.endTime - this.startTime;
+            this.total += this.duration;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            while (true)
+            {
+                Console.WriteLine("Type '{0}' or 'quit to quit: ", stopwatch.startOrStop);
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "start" && stopwatch.running == false)
+                {
+                    stopwatch.Start();
+                    continue;
+                }
+                else if (input.ToLower() == "stop" && stopwatch.running == true)
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine(stopwatch.total);
+                    continue;
+                }
+                else if (input.ToLower() == "quit")
+                    break;
+            }
+        }
+    }
+}
