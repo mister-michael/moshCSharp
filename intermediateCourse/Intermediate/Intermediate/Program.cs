b1e87613b4de0e6e70a7f98279e87643ea152ab5
Michael Clark <michaelclarknashville@protonmail.com>
2021-04-27 10:58:20 -0500

    Stack List of objects, with push pop and clear method

--- intermediateCourse/Intermediate/Intermediate/Program.cs
+++ intermediateCourse/Intermediate/Intermediate/Program.cs
@@ -1,33 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Intermediate
 {
-    class Program
+    class Stack
     {
-        public static void Main(string[] args)
+        private List<object> _list = new List<object>();
+
+        
+        public void Push(object obj)
         {
-            var stopwatch = new StopWatch();
+            if (obj == null)
+                throw new InvalidOperationException("You can't pass a null object");
 
-            while (true)
+            _list.Add(obj);
+
+        }
+
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException("list is empty");
+
+            object toReturn = _list[_list.Count - 1];
+
+            var listElements = _list.Count;
+
+            _list.RemoveAt(listElements - 1);
+
+            return toReturn;
+
+        }
+
+        public void Clear()
+        {
+            while(_list.Count < 0)
             {
-                Console.WriteLine("Type '{0}' or 'quit to quit: ", stopwatch.startOrStop);
-                var input = Console.ReadLine();
-
-                if (input.ToLower() == "start" && stopwatch.running == false)
-                {
-                    stopwatch.Start();
-                    continue;
-                }
-                else if (input.ToLower() == "stop" && stopwatch.running == true)
-                {
-                    stopwatch.Stop();
-                    Console.WriteLine(stopwatch.total);
-                    continue;
-                }
-                else if (input.ToLower() == "quit")
-                    break;
+                _list.RemoveAt(0);
             }
         }
-    }
 
+        public static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            int ini = 0;
+            int max = 10;
+
+            for (var i = ini; i <= max; i++)
+                stack.Push(i);
+
+            for (var i = ini; i <= max; i++)
+                Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+
+            Console.WriteLine("Counht:" + stack._list.Count);
+
+            Console.ReadLine();
+        }
+    }
 }
+
+
